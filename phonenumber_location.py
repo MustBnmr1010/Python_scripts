4e5acaf3ae8dc8ecae523073e1012cacbc87df6a
MustBnmr1010 <99210153+MustBnmr1010@users.noreply.github.com>
2022-02-07 20:11:46 +0100

    Update phonenumber_location.py

--- phonenumber_location.py
+++ phonenumber_location.py
@@ -1,8 +1,8 @@
 import phonenumbers
-#idir dnt forget the country code 
+#dnt forget the country code 
 number = input("Your number or other number : ")
 
-key = "c347aa4f572a4c58b509845f65b23829"
+key = "gp to opencagedata and make an account and get the api key."
 
 from phonenumbers import geocoder
 
@@ -15,8 +15,6 @@ from phonenumbers import carrier
 service_number = phonenumbers.parse(number, "RO")
 
 print(carrier.name_for_number(service_number, "en"))
-
-#Now lets find the exct location "must-to-idir"
 
 from opencage.geocoder import OpenCageGeocode
 
@@ -35,7 +33,8 @@ name = number+".html"
 myMap = folium.Map(location=[lat, lang], zoom_start=9)
 folium.Marker([lat, lang],popup=Location).add_to((myMap))
 myMap.save(name)
-lat_lng = lat+", "+lang
-link = f'https://www.google.com/maps/@{lat_lng}z?hl=fr'
+lat_lng = lat+lang
+v = ","
+link = f'https://www.google.com/maps/@{lat+v+lang}z?hl=fr'
 print("Check ur directory and enter this file : "+name)
 print(f"NOTE! If the {name} didnt load perfectly go to this link : ",link)
